22933120c9f5a260c5ecd1caa20c19ed9cf7a7b4
damianckziu <Damian.Fital@gmail.com>
2026-03-21 22:27:29 +0100

    Death

--- arena.gd
+++ arena.gd
@@ -11,7 +11,7 @@ func _exit_tree():
 func _on_enemy_spawn_timer_timeout():
 	var enemy_position = Vector2(randf_range(-160, 670), randf_range(-90, 390))
 	
-	while enemy_position.x < 640 and enemy_position.x > -80 or enemy_position.y < 360 and enemy_position.y > -45:
+	while enemy_position.x < 640 and enemy_position.x > -80 and enemy_position.y < 360 and enemy_position.y > -45:
 		enemy_position = Vector2(randf_range(-160, 670), randf_range(-90, 390))
 	
 	Global.instance_node(enemy_1, enemy_position, self)
--- player.gd
+++ player.gd
@@ -6,6 +6,7 @@ var velocity = Vector2()
 var bullet = preload("res://bullet.tscn")
 
 var can_shoot = true
+var is_dead = false
 
 func _ready():
 	Global.player = self
@@ -19,12 +20,20 @@ func _process(delta):
 	
 	velocity = velocity.normalized()
 	
-	global_position += speed * velocity * delta
+	if is_dead == false:
+		global_position += speed * velocity * delta
 	
-	if Input.is_action_pressed("click") and Global.node_creation_parent != null and can_shoot:
+	if Input.is_action_pressed("click") and Global.node_creation_parent != null and can_shoot and is_dead == false:
 		Global.instance_node(bullet, global_position, Global.node_creation_parent)
 		$Reload_speed.start()
 		can_shoot = false
 
 func _on_Reload_speed_timeout():
 	can_shoot = true
+
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("Enemy"):
+		is_dead = true
+		visible = false
+		await get_tree().create_timer(1).timeout
+		get_tree().reload_current_scene()
