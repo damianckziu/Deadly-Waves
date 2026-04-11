44ce4ca89d5dc1353347d8efbc0851a5a2893168
damianckziu <Damian.Fital@uczen.ckziu.net>
2026-04-11 18:14:41 +0200

    FALE

--- arena.gd
+++ arena.gd
@@ -1,46 +1,57 @@
 extends Node2D
-
 var enemy_1 = preload("res://enemy.tscn")
-
+var current_wave = 0
+var enemies_alive = 0
+var spawning = false
 func _ready():
 	Global.node_creation_parent = self
 	Global.points = 0
 	Global.arena = self
 	$PauseMenu.visible = false
-
+	start_next_wave()
 func _exit_tree():
 	Global.node_creation_parent = null
-
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if get_tree().paused:
 			_resume()
 		else:
 			_pause()
-
 func _pause():
 	$PauseMenu.visible = true
 	get_tree().paused = true
-
 func _resume():
 	$PauseMenu.visible = false
 	get_tree().paused = false
-
-func _on_enemy_spawn_timer_timeout():
-	var enemy_position = Vector2(randf_range(-160, 670), randf_range(-90, 390))
+func start_next_wave():
+	current_wave += 1
+	spawning = true
 	
+	$HUD/WaveLabel.text = "FALA " + str(current_wave)
+	$HUD/WaveLabel.visible = true
+	await get_tree().create_timer(2.0).timeout
+	$HUD/WaveLabel.visible = false
+	
+	var enemies_to_spawn = 3 + (current_wave * 2)
+	enemies_alive = enemies_to_spawn
+	
+	for i in range(enemies_to_spawn):
+		spawn_enemy()
+		await get_tree().create_timer(0.3).timeout
+	
+	spawning = false
+func spawn_enemy():
+	var enemy_position = Vector2(randf_range(-160, 670), randf_range(-90, 390))
 	while enemy_position.x < 640 and enemy_position.x > -80 and enemy_position.y < 360 and enemy_position.y > -45:
 		enemy_position = Vector2(randf_range(-160, 670), randf_range(-90, 390))
-	
 	Global.instance_node(enemy_1, enemy_position, self)
-
-func _on_difficulty_timer_timeout():
-	if $Enemy_spawn_timer.wait_time > 0.5:
-		$Enemy_spawn_timer.wait_time -= 0.1
-
+func enemy_died():
+	enemies_alive -= 1
+	if enemies_alive <= 0 and spawning == false:
+		await get_tree().create_timer(1.5).timeout
+		start_next_wave()
 func _on_wroc_pressed():
 	_resume()
-
 func _on_menu_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://menu.tscn")
--- enemy.gd
+++ enemy.gd
@@ -1,12 +1,8 @@
 extends Sprite2D
-
 var speed = 75
-
 var velocity = Vector2()
-
 var stun = false
 var hp = 3
-
 var blood_particles = preload("res://blood_particles.tscn")
 
 func _process(delta):
@@ -21,6 +17,7 @@ func _process(delta):
 		Global.points += 10
 		if Global.arena != null:
 			Global.arena.get_node("DeathSound").play()
+			Global.arena.enemy_died()
 		if Global.node_creation_parent != null:
 			var blood_particles_instance = Global.instance_node(blood_particles, global_position, Global.node_creation_parent)
 			blood_particles_instance.rotation = velocity.angle()
